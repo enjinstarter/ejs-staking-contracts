acec541473131848e37f151ee765cc164a2c4296
Tim Loh <timloh@Tims-MacBook-Pro.local>
2024-03-01 17:27:29 +0800

    chore: linting

--- contracts/libraries/UnitConverter.sol
+++ contracts/libraries/UnitConverter.sol
@@ -18,16 +18,12 @@ library UnitConverter {
      */
     // https://github.com/crytic/slither/wiki/Detector-Documentation#dead-code
     // slither-disable-next-line dead-code
-    function scaleWeiToDecimals(uint256 weiAmount, uint256 decimals)
-        internal
-        pure
-        returns (uint256 decimalsAmount)
-    {
+    function scaleWeiToDecimals(uint256 weiAmount, uint256 decimals) internal pure returns (uint256 decimalsAmount) {
         require(decimals <= TOKEN_MAX_DECIMALS, "UnitConverter: decimals");
 
         if (decimals < TOKEN_MAX_DECIMALS && weiAmount > 0) {
             uint256 decimalsDiff = TOKEN_MAX_DECIMALS - decimals;
-            decimalsAmount = weiAmount / 10**decimalsDiff;
+            decimalsAmount = weiAmount / 10 ** decimalsDiff;
         } else {
             decimalsAmount = weiAmount;
         }
@@ -41,16 +37,12 @@ library UnitConverter {
      */
     // https://github.com/crytic/slither/wiki/Detector-Documentation#dead-code
     // slither-disable-next-line dead-code
-    function scaleDecimalsToWei(uint256 decimalsAmount, uint256 decimals)
-        internal
-        pure
-        returns (uint256 weiAmount)
-    {
+    function scaleDecimalsToWei(uint256 decimalsAmount, uint256 decimals) internal pure returns (uint256 weiAmount) {
         require(decimals <= TOKEN_MAX_DECIMALS, "UnitConverter: decimals");
 
         if (decimals < TOKEN_MAX_DECIMALS && decimalsAmount > 0) {
             uint256 decimalsDiff = TOKEN_MAX_DECIMALS - decimals;
-            weiAmount = decimalsAmount * 10**decimalsDiff;
+            weiAmount = decimalsAmount * 10 ** decimalsDiff;
         } else {
             weiAmount = decimalsAmount;
         }
